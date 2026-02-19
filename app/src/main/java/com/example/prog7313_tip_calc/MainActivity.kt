6788d80e6e6241588f4848abf726555d8a857b3d
Genius Muzama <117445671+ST10027393@users.noreply.github.com>
2026-02-19 09:13:05 +0200

    Split bill working

--- app/src/main/java/com/example/prog7313_tip_calc/MainActivity.kt
+++ app/src/main/java/com/example/prog7313_tip_calc/MainActivity.kt
@@ -16,36 +16,46 @@ class MainActivity : AppCompatActivity() {
         //variables
         val calcTip = findViewById<Button>(R.id.button)
         val inputAmount = findViewById<EditText>(R.id.edit_text)
-        val question = findViewById<TextView>(R.id.splitQuestion)
+        //val question = findViewById<TextView>(R.id.splitQuestion)
         val finalBill = findViewById<TextView>(R.id.billReport)
         val yesButton = findViewById<RadioButton>(R.id.yesButton)
         val noButton = findViewById<RadioButton>(R.id.noButton)
 
         //Calculate tip
+        //Calculate tip
         calcTip.setOnClickListener {
             val inputString = inputAmount.text.toString()
-            val tip = 0.1;
-            val billValue = inputString.toDouble()
-            if (inputString.isNotEmpty() && noButton.isActivated) {
-                val tipAmount = billValue * tip;
-                var finalAmount = tipAmount + billValue;
-                finalBill.text = String.format("\nSplit Bill: No" +
-                        "\nTip 1: %.2f $tipAmount" +
-                        "\nTip 2: N/A" +
-                        "\nTotal: %.2f $finalAmount")
-            } else if(inputString.isNotEmpty() && yesButton.isActivated){
-                val splitBill = billValue * 0.5
-                val tipValue = splitBill * tip
-                var individualAmount = tipValue + splitBill
-                var finalAmount = individualAmount * 2
-                finalBill.text = String.format("\nSplit Bill: Yes" +
-                        "\nTip 1: %.2f $tipValue" +
-                        "\nTip 2: %.2f $tipValue" +
-                        "\nTotal: %.2f $finalAmount")
-            }else {
-                // Optional: Tell the user they forgot to enter a number
-                Toast.makeText(this, "Please enter a value", Toast.LENGTH_SHORT).show()
-            }//else end
+            val tip = 0.1
+
+            if (inputString.isNotEmpty()) {
+                val billValue = inputString.toDouble()
+                if (noButton.isChecked) {
+                    val tipAmount = billValue * tip
+                    val finalAmount = tipAmount + billValue
+
+                    finalBill.text = String.format(
+                        "\nSplit Bill: No\nTip 1: R %.2f\nTip 2: N/A\nTotal: R %.2f",
+                        tipAmount, finalAmount
+                    )
+
+                } else if (yesButton.isChecked) {
+                    val splitBill = billValue * 0.5
+                    val tipValue = splitBill * tip
+                    val individualAmount = tipValue + splitBill
+                    val finalAmount = individualAmount * 2
+
+                    finalBill.text = String.format(
+                        "\nSplit Bill: Yes\nTip 1: R %.2f\nTip 2: R %.2f\nTotal: R %.2f",
+                        tipValue, tipValue, finalAmount
+                    )
+
+                } else {
+                    Toast.makeText(this, "Please select whether to split the bill", Toast.LENGTH_SHORT).show()
+                }
+
+            } else {
+                Toast.makeText(this, "Please enter a bill amount", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 }
